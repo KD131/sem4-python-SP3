7c6a32515ff03e9d90828521c5d29337b3088f20
KD131 <71063280+KD131@users.noreply.github.com>
2022-05-01 23:42:06 +0200

    plot stuff

--- diabetes.py
+++ diabetes.py
@@ -1,7 +1,8 @@
-from sklearn.datasets import load_diabetes
-from sklearn.linear_model import LinearRegression
+import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
+from sklearn.datasets import load_diabetes
+from sklearn.linear_model import LinearRegression
 
 dataset = load_diabetes() # I would use as_frame=True, but I think the Docker env doesn't have the right update for it.
 df = pd.DataFrame(dataset.data, columns=dataset.feature_names)
@@ -14,4 +15,10 @@ model = LinearRegression()
 model.fit(xs_reshape, ys)
 print(f'Model: y = {model.coef_[0]}x + {model.intercept_}')
 
+plt.figure()
+plt.scatter(xs, ys, s=20)
+plt.plot(xs, model.predict(xs_reshape), c='k')
+plt.show()
 
+x = 2
+print(f'BP for BMI={x}: {model.predict([[x]])[0]}')
