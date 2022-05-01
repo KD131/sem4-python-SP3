ac221b62e2e22d881ac9fdabe32de8cd67913026
KD131 <71063280+KD131@users.noreply.github.com>
2022-05-01 23:50:31 +0200

    refactor

--- diabetes.py
+++ diabetes.py
@@ -4,21 +4,30 @@ import pandas as pd
 from sklearn.datasets import load_diabetes
 from sklearn.linear_model import LinearRegression
 
-dataset = load_diabetes() # I would use as_frame=True, but I think the Docker env doesn't have the right update for it.
-df = pd.DataFrame(dataset.data, columns=dataset.feature_names)
-# print(df.head())
-xs = df['bmi']
-xs_reshape = np.array(xs).reshape(-1 ,1)
-ys = df['bp']
 
-model = LinearRegression()
-model.fit(xs_reshape, ys)
-print(f'Model: y = {model.coef_[0]}x + {model.intercept_}')
+def get_model(xs_reshape, ys):
+    model = LinearRegression()
+    model.fit(xs_reshape, ys)
+    print(f'Model: y = {model.coef_[0]}x + {model.intercept_}')
+    return model
 
-plt.figure()
-plt.scatter(xs, ys, s=20)
-plt.plot(xs, model.predict(xs_reshape), c='k')
-plt.show()
+def plot_model_and_data(xs, ys, model, xs_reshape):
+    plt.figure()
+    plt.scatter(xs, ys, s=20)
+    plt.plot(xs, model.predict(xs_reshape), c='k')
+    plt.show()
 
-x = 2
-print(f'BP for BMI={x}: {model.predict([[x]])[0]}')
+def predict_for_x(model, x):
+    print(f'BP for BMI={x}: {model.predict([[x]])[0]}')
+
+if __name__ == '__main__':
+    dataset = load_diabetes() # I would use as_frame=True, but I think the Docker env doesn't have the right update for it.
+    df = pd.DataFrame(dataset.data, columns=dataset.feature_names)
+    # print(df.head())
+    xs = df['bmi']
+    xs_reshape = np.array(xs).reshape(-1 ,1)
+    ys = df['bp']
+
+    model = get_model(xs_reshape, ys)
+    plot_model_and_data(xs, ys, model, xs_reshape)
+    predict_for_x(model, 2)
