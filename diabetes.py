516e04509dc28e43c30a04d1a07d90dc942cb746
KD131 <71063280+KD131@users.noreply.github.com>
2022-05-01 23:23:03 +0200

    make regression model

--- diabetes.py
+++ diabetes.py
@@ -1,10 +1,17 @@
 from sklearn.datasets import load_diabetes
+from sklearn.linear_model import LinearRegression
+import numpy as np
 import pandas as pd
 
 dataset = load_diabetes() # I would use as_frame=True, but I think the Docker env doesn't have the right update for it.
 df = pd.DataFrame(dataset.data, columns=dataset.feature_names)
 # print(df.head())
 xs = df['bmi']
+xs_reshape = np.array(xs).reshape(-1 ,1)
 ys = df['bp']
+
+model = LinearRegression()
+model.fit(xs_reshape, ys)
+print(f'Model: y = {model.coef_[0]}x + {model.intercept_}')
 
 
